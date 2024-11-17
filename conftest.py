724ed15fad6a10816b5aeac98885c94123ac4a86
Andrei Monichev <vyroud@gmail.com>
2024-11-17 21:46:15 +0300

    Fixed typos

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selene import browser, be, have
 
 @pytest.fixture(scope="session")
 def browser_set():
-    browser.open("htttps://gooogle.com/ncr")
+    browser.open("https://gooogle.com/ncr")
     browser.driver.maximize_window()
     yield
     browser.quit()
