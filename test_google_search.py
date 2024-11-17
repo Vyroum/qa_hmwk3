f47ef43a864d407b362b0e1735c983ee5b4f2476
Andrei Monichev <vyroud@gmail.com>
2024-11-17 20:57:28 +0300

    Variable testing

--- test_google_search.py
+++ test_google_search.py
@@ -2,11 +2,11 @@ import pytest
 from selene import browser, be, have
 
 @pytest.fixture
-def search_for_result(browser_set):
+def search_for_result(test_browser_set):
     browser.element('[name="q"').should(be.blank).type("Pytest").press_enter()
     browser.element('[id="search"]').should(have.text("pytest documentation"))
 
-@pytest.fixture()
-def search_for_no_result(browser_set):
+@pytest.fixture
+def search_for_no_result(test_browser_set):
     browser.element('[name="q"').should(be.blank).type("asfa12312azxeahgaewhqawhyg").press_enter()
-    browser.element('[id="search"]').should(have.text("ничего не найдено"))
+    browser.element('[id="search"]').should(have.text(" ничего не найдено"))
