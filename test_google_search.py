8ed3148d70c9c41591878324e3eec384e45517e2
Andrei Monichev <vyroud@gmail.com>
2024-11-17 20:47:55 +0300

    Added tests

---
+++ test_google_search.py
@@ -0,0 +1,12 @@
+import pytest
+from selene import browser, be, have
+
+@pytest.fixture
+def search_for_result(browser_set):
+    browser.element('[name="q"').should(be.blank).type("Pytest").press_enter()
+    browser.element('[id="search"]').should(have.text("pytest documentation"))
+
+@pytest.fixture()
+def search_for_no_result(browser_set):
+    browser.element('[name="q"').should(be.blank).type("asfa12312azxeahgaewhqawhyg").press_enter()
+    browser.element('[id="search"]').should(have.text("ничего не найдено"))
