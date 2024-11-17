ed414335341a3bb5aaf6445e798c746c0ad14164
Andrei Monichev <vyroud@gmail.com>
2024-11-17 22:27:25 +0300

    Fixed errors and fixtures

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
 import pytest
-from selene import browser, be, have
+from selene import browser
 
 @pytest.fixture(scope="session")
 def browser_set():
-    browser.open("https://gooogle.com/ncr")
+    browser.open("https://google.com/")
     browser.driver.maximize_window()
     yield
     browser.quit()
--- test_google_search.py
+++ test_google_search.py
@@ -1,12 +1,13 @@
 import pytest
 from selene import browser, be, have
 
-@pytest.fixture
-def test_search_for_result(test_browser_set):
+
+def test_search_for_result(browser_set):
     browser.element('[name="q"').should(be.blank).type("Pytest").press_enter()
     browser.element('[id="search"]').should(have.text("pytest documentation"))
+    browser.open("https://google.com/")
 
-@pytest.fixture
-def test_search_for_no_result(test_browser_set):
+
+def test_search_for_no_result(browser_set):
     browser.element('[name="q"').should(be.blank).type("asfa12312azxeahgaewhqawhyg").press_enter()
-    browser.element('[id="search"]').should(have.text(" ничего не найдено"))
+    browser.element('[aria-level="3"]').should(have.text(" ничего не найдено"))
